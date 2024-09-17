07aa0de4464b62f231cdcd4354cb2caa885f6445
Kareem Helany <kareem.helany@wikendu.de>
2024-09-17 21:41:20 +0200

    #1: Add Methode erstellt

--- Oberfläche/Program.cs
+++ Oberfläche/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Oberfläche.UI;
+
+BenutzerOberFläche benutzerOberFläche = new BenutzerOberFläche();
+benutzerOberFläche.Add();
---
+++ Oberfläche/UI/BenutzerOberFläche.cs
@@ -0,0 +1,20 @@
+﻿namespace Oberfläche.UI
+{
+    public class BenutzerOberFläche
+    {
+        string produkt;
+        public void Add()
+        {
+            Console.WriteLine("Produkt Hinzufügen:");
+            produkt = Console.ReadLine();
+
+            Console.WriteLine("Das Produkt " + produkt + " wurde erfolgreich hinzugefügt!");
+
+
+        }
+
+
+
+
+    }
+}
