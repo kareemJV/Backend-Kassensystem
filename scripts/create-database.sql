3a3ed3d26a20df61e04d9a65549cd67d7674b66f
Kareem Helany <kareem.helany@wikendu.de>
2024-09-18 18:27:47 +0200

    #1: Docker eingerichtet

---
+++ scripts/create-database.sql
@@ -0,0 +1,6 @@
+-- Erstellen einer neuen Datenbank mit dem Namen MyNewDatabase
+CREATE DATABASE Products;
+GO
+
+-- Optional: Anzeigen der neu erstellten Datenbank
+-- SELECT name FROM sys.databases WHERE name = 'MyNewDatabase';
